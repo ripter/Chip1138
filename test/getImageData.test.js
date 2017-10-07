4464b3aa552293a2493d7ae4804fd2638f40a29c
skipzero <skip@angerbunny.com>
2017-10-06 21:28:17 -0700

    code good, tests bad...

--- test/getImageData.test.js
+++ test/getImageData.test.js
@@ -1,21 +1,22 @@
 const expect = require('expect.js');
 const getImageData = require('../src/getImageData.js');
 
-console.log(getImageData)
 describe('parse image:', () => {
-  let actual, dataURL;
+  let dataURL;
 
   describe('return 1x1 Red Square', () => {
     beforeEach(() => {
       dataURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2P4z8DwHwAFAAH/plybXQAAAABJRU5ErkJggg==';
-      actual = getImageData(dataURL);
     });
 
-    it('ImageData', () => {
-      expect(actual).to.eql({
-        data: [255, 0, 0, 255],
-        width: 1,
-        height: 1,
+    it('ImageData', (done) => {
+      getImageData(dataURL).then((actual) => {
+        expect(actual).to.eql({
+          data: [255, 0, 0, 255],
+          width: 1,
+          height: 1,
+        });
+        done();
       });
     });
 
