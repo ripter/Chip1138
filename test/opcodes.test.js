c74d346fb37ce2f975baa0eac7b55bd73ccbf59d
Chris Richards <ripter001@gmail.com>
2017-10-13 21:52:33 -0700

    reading opcode from file

--- test/opcodes.test.js
+++ test/opcodes.test.js
@@ -1,6 +1,7 @@
 import expect from 'expect.js';
+import filter from 'lodash.filter';
 import CPU from '../src/cpu.js';
-import { OPCODE } from '../const/asm.js';
+import OPCODE from '../const/opcode.js';
 
 
 describe('opcodes', () => {
@@ -10,41 +11,90 @@ describe('opcodes', () => {
     cpu = new CPU();
   });
 
-  describe('LD', () => {
-    it('B, 0x1c ; loads 0x1c into register b', () => {
-      cpu.processOpcode(0x06);
-      cpu.processOpcode(0x1c);
-      expect(cpu.b).to.eql(0x1c);
+  describe('', () => {
+    let loadOpcodes = filter(OPCODE, {mnemonic: 'LD'});
+    // let opcodes = {
+    //   0x06: ['b'],
+    //   0x0e: ['c'],
+    //   0x16: ['d'],
+    //   0x1e: ['e'],
+    //   0x26: ['h'],
+    //   0x2e: ['l'],
+    //   0x75: ['hl'],
+    // };
+
+    // Load Value into Register
+    loadOpcodes.forEach(function(item) {
+      const randomByte = 0|Math.random()*256;
+      const { addr, length, mnemonic, operand1 } = item;
+
+      if (length === 1) {
+        it(`${mnemonic} ${operand1}, ${randomByte}; loads ${randomByte} into register ${operand1}`, () => {
+          cpu.processOpcode(addr);
+          cpu.processOpcode(randomByte);
+          expect(cpu[operand1]).to.eql(randomByte);
+        });
+      }
     });
 
-    it('C, 0xcf; loads 0xcf into register c', () => {
-      cpu.processOpcode(0x0e);
-      cpu.processOpcode(0xcf);
-      expect(cpu.c).to.eql(0xcf);
-    });
-
-    it('D, 0xa8; loads 0xa8 into register d', () => {
-      cpu.processOpcode(0x16);
-      cpu.processOpcode(0xa8);
-      expect(cpu.d).to.eql(0xa8);
-    });
-
-    it('E, 0xbb; loads 0xbb into register e', () => {
-      cpu.processOpcode(0x1e);
-      cpu.processOpcode(0xbb);
-      expect(cpu.e).to.eql(0xbb);
-    });
-
-    it('H, 0x06; loads 0x06 into register h', () => {
-      cpu.processOpcode(0x26);
-      cpu.processOpcode(0x06);
-      expect(cpu.h).to.eql(0x06);
-    });
-
-    it('L, 0x16; loads 0x16 into register l', () => {
-      cpu.processOpcode(0x2e);
-      cpu.processOpcode(0x16);
-      expect(cpu.l).to.eql(0x16);
-    });
+    // Load Register into Register
+    // opcodes = {
+    //   0x7f: ['a', 'a'],
+    //   0x78: ['a', 'b'],
+    //   0x79: ['a', 'c'],
+    //   0x7a: ['a', 'd'],
+    //   0x7b: ['a', 'e'],
+    //   0x7c: ['a', 'h'],
+    //   0x7d: ['a', 'l'],
+    //   0x7e: ['a', 'hl'],
+    //   0x40: ['b', 'b'],
+    //   0x41: ['b', 'c'],
+    //   0x42: ['b', 'd'],
+    //   0x43: ['b', 'e'],
+    //   0x44: ['b', 'h'],
+    //   0x45: ['b', 'l'],
+    //   0x46: ['b', 'hl'],
+    //   0x48: ['c', 'b'],
+    //   0x49: ['c', 'c'],
+    //   0x4a: ['c', 'd'],
+    //   0x4b: ['c', 'e'],
+    //   0x4c: ['c', 'h'],
+    //   0x4d: ['c', 'l'],
+    //   0x4e: ['c', 'hl'],
+    //   0x50: ['d', 'b'],
+    //   0x51: ['d', 'c'],
+    //   0x52: ['d', 'd'],
+    //   0x53: ['d', 'e'],
+    //   0x54: ['d', 'h'],
+    //   0x55: ['d', 'l'],
+    //   0x56: ['d', 'hl'],
+    //   0x58: ['e', 'b'],
+    //   0x59: ['e', 'c'],
+    //   0x5a: ['e', 'd'],
+    //   0x5b: ['e', 'e'],
+    //   0x5c: ['e', 'h'],
+    //   0x5d: ['e', 'l'],
+    //   0x5e: ['e', 'hl'],
+    //   0x60: ['h', 'b'],
+    //   0x61: ['h', 'c'],
+    //   0x62: ['h', 'd'],
+    //   0x63: ['h', 'e'],
+    //   0x64: ['h', 'h'],
+    //   0x65: ['h', 'l'],
+    //   0x66: ['h', 'hl'],
+    //   0x68: ['l', 'b'],
+    //   0x69: ['l', 'c'],
+    //   0x6a: ['l', 'd'],
+    //   0x6b: ['l', 'e'],
+    //   0x6c: ['l', 'h'],
+    //   0x6d: ['l', 'l'],
+    //   0x6e: ['l', 'hl'],
+    //   0x70: ['hl', 'b'],
+    //   0x71: ['hl', 'c'],
+    //   0x72: ['hl', 'd'],
+    //   0x73: ['hl', 'e'],
+    //   0x74: ['hl', 'h'],
+    //   0x75: ['hl', 'l'],
+    // };
   }); // LD
 });
