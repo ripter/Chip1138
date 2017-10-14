d329e31006e9e002ac74b185864890583ac93721
Chris Richards <ripter001@gmail.com>
2017-10-13 19:06:03 -0700

    fixed up tests to use const

--- const/asm.js
+++ const/asm.js
@@ -1,26 +1,32 @@
-export const LD {
-
-};
-
-export const opcodes = {
+export const OPCODE = {
    0x06: {
      asm: 'LD B, d8; load the next 8bit value into register b',
-     parameterCount: 1,
+     argLength: 1,
      cycles: 8,
    },
    0x0e: {
      asm: 'LD C, d8; load the next 8bit value into register c',
-     parameterCount: 1,
+     argLength: 1,
      cycles: 8,
    },
    0x16: {
      asm: 'LD D, d8; load the next 8bit value into register d',
-     parameterCount: 1,
+     argLength: 1,
      cycles: 8,
    },
    0x1e: {
      asm: 'LD E, d8; load the next 8bit value into register e',
-     parameterCount: 1,
+     argLength: 1,
      cycles: 8,
    },
+   0x26: {
+     asm: 'LD H, d8; load the next 8bit value into register h',
+     argLength: 1,
+     cycles: 8,
+   },
+   0x2e: {
+     asm: 'LD L, d8; load the next 8bit value into register l',
+     argLength: 1,
+     cycles: 8,
+   }
 };
