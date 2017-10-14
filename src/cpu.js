b40fe492a515c9523b0bcce1703dd3d332f3bbee
skipzero <skip@angerbunny.com>
2017-10-13 17:47:08 -0700

    all registers passing tests!

--- src/cpu.js
+++ src/cpu.js
@@ -5,7 +5,7 @@ class CPU {
   constructor() {
     // Create the memory banks
     this.memory8bit = new Uint8Array(18);
-    this.memory16bit = new Uint16Array(4);
+    this.memory16bit = new Uint16Array(5);
   }
 
   get a() {
@@ -70,5 +70,112 @@ class CPU {
   set h(value) {
     this.memory8bit[7] = value;
   }
+
+  get altA() {
+    return this.memory8bit[8];
+  }
+
+  set altA(value) {
+    this.memory8bit[8] = value;
+  }
+
+  get altB() {
+    return this.memory8bit[9];
+  }
+
+  set altB(value) {
+    this.memory8bit[9] = value;
+  }
+
+  get altD() {
+    return this.memory8bit[10];
+  }
+
+  set altD(value) {
+    this.memory8bit[10] = value;
+  }
+
+  get altH() {
+    return this.memory8bit[11];
+  }
+
+  set altH(value) {
+    this.memory8bit[11] = value;
+  }
+
+  get altF() {
+    return this.memory8bit[12];
+  }
+
+  set altF(value) {
+    this.memory8bit[12] = value;
+  }
+
+  get altC() {
+    return this.memory8bit[13];
+  }
+
+  set altC(value) {
+    this.memory8bit[13] = value;
+  }
+
+  get altE() {
+    return this.memory8bit[14];
+  }
+
+  set altE(value) {
+    this.memory8bit[14] = value;
+  }
+
+  get altL() {
+    return this.memory8bit[15];
+  }
+
+  set altL(value) {
+    this.memory8bit[15] = value;
+  }
+
+  get i () {
+    return this.memory16bit[0];
+  }
+
+  set i (value) {
+    this.memory16bit[0] = value;
+  }
+
+  get r () {
+    return this.memory16bit[1];
+  }
+
+  set r (value) {
+    this.memory16bit[1] = value;
+  }
+
+  get ix () {
+    return this.memory16bit[2];
+  }
+
+  set ix (value) {
+    this.memory16bit[2] = value;
+  }
+
+  get iy () {
+    return this.memory16bit[3];
+  }
+
+  set iy (value) {
+    this.memory16bit[3] = value;
+  }
+
+  get sp () {
+    return this.memory16bit[4];
+  }
+
+  set sp (value) {
+    this.memory16bit[4] = value;
+  }
+
+
 }
+
 export default CPU;
