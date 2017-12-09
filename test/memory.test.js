13b2b00748b8a6353c97cb2d422fb998fcf85888
Chris Richards <ripter001@gmail.com>
2017-12-09 00:44:23 -0800

    adding more tests

--- test/memory.test.js
+++ test/memory.test.js
@@ -1,12 +1,29 @@
 import expect from 'expect.js';
 import Memory from '../src/memory.js';
 import { random8bit } from './utils.js';
+import rom from '../roms/flappyboy.json';
 
-describe('Memory', () => {
+describe.only('Memory', () => {
   let memory;
 
   beforeEach(() => {
-    memory = new Memory();
-  })
+    memory = new Memory(rom);
+  });
 
+  it('requires a ROM', () => {
+    expect(function() {
+      new Memory();
+    }).to.throwException(/requires ROM/);
+  });
+
+  it('can read from the ROM', () => {
+    const actual = memory.readROM(0x0000);
+    expect(actual).to.eql(0xc9);
+  });
+
+  it('can read ROM TITLE', () => {
+    const actual = memory.readROMRange(0x0134, 0x142);
+    const expected = new new Uint8Array([0x46, 0x4c, 0x41, 0x50, 0x50, 0x59, 0x42, 0x4f, 0x59]);
+    expect(actual).to.eql(expected);
+  });
 });
