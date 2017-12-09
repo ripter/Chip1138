33214e0611c8fa9bb3dc0b8750c0183f79074c27
Chris Richards <ripter001@gmail.com>
2017-12-08 18:54:32 -0800

    added small utility that can turn genuine binary files into json base64 encoded datauri

---
+++ test/memory.test.js
@@ -0,0 +1,12 @@
+import expect from 'expect.js';
+import Memory from '../src/memory.js';
+import { random8bit } from './utils.js';
+
+describe('Memory', () => {
+  let memory;
+
+  beforeEach(() => {
+    memory = new Memory();
+  })
+
+});
