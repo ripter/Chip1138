e765c6f5179e7cd063cfe6abec057356c532d785
skipzero <skip@angerbunny.com>
2017-12-16 17:52:36 -0800

    MVP for throwing error

--- src/memory.js
+++ src/memory.js
@@ -5,14 +5,9 @@ class Memory {
 
   constuctor(rom) {
 
-    const error = () => {
-
       if (!rom) {
         throw new Error('requires ROM');
       }
-      this.data = loadROM(rom);
-    }
-    error(rom);
   }
 
   readROM() {
