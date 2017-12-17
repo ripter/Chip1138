365d937b03ecf363b3011d264406088a7f18a2a7
skipzero <skip@angerbunny.com>
2017-12-17 15:38:22 -0800

    fixed some of the coments, added more documentation

--- src/memory.js
+++ src/memory.js
@@ -12,27 +12,35 @@ class Memory {
     this.data = loadROM(rom);
   }
 
-  readROM(value) {
+  readROM(address) {
     let romValue;
-    if (typeof value != 'number') {
-      romValue = this.data;
+    // if we used `if (!value)...` we would get a hit on 0 since it's falsey.
+    if (typeof address != 'number') {
+      romValue = this.data; // returns the whole ROM loaded in
     }
     else {
-      romValue = this.data[value];
+      romValue = this.data[address];
     }
     return romValue;
   }
 
-  readROMRange(range) {
+  readROMRange(rangeLow, rangeHigh) {
     // range to read from
-    if (!range) {
+    if (!rangeLow && !rangeHigh) {
       throw new Error('please provide a range to return');
     }
-    const low = arguments[0];
-    const high = arguments[1];
+    // smallest number of our range
+    const low = rangeLow;
+    // largest number of our range
+    const high = rangeHigh;
+    // get our new Array length
     const length = high - low + 1;
+
+    // take our range from the ROM data
     const tempArray = this.data.slice(low, high + 1);
+    // create out new typed array
     let rangeArray = new Uint8Array(length);
+    // set the values of tempArray into rangeArray
     rangeArray.set(tempArray);
     return rangeArray;
   }
