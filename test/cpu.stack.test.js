4c6dc78b01099eebf991fde27be65a8755252827
Chris Richards <ripter001@gmail.com>
2018-01-07 12:46:42 -0800

    added tests for stack pointer

--- test/cpu.stack.test.js
+++ test/cpu.stack.test.js
@@ -3,12 +3,16 @@ import expect from 'expect.js';
 import CPU from '../src/cpu.js';
 import Memory from '../src/memory.js';
 import { random8bit } from './utils.js';
+import rom from '../roms/flappyboy.json';
 
 describe('CPU Stack', () => {
-  let cpu;
+  let cpu, memory;
 
   beforeEach(() => {
-    cpu = new CPU();
+    memory = new Memory(rom);
+    cpu = new CPU({
+      memory,
+    });
   });
 
   /*
@@ -18,9 +22,25 @@ describe('CPU Stack', () => {
   memory address 1005h contains 33h,
   and the Stack Pointer contains 1005h.
    */
-  it('spec example', () => {
-    cpu.af = 0x2233;
-    cpu.sp = 0x1007;
-    cpu.processOpcode(0xf5);
+  describe('spec example', () => {
+    beforeEach(() => {
+      cpu.af = 0x2233;
+      cpu.sp = 0x1007;
+      cpu.processOpcode(0xf5);
+    });
+
+    it('value at address 0x1006 is 0x22', () => {
+      const actual = memory.readROM(0x1006);
+      expect(actual).to.eql(0x22);
+    });
+
+    it('value at address 0x1005 is 0x33', () => {
+      const actual = memory.readROM(0x1005);
+      expect(actual).to.eql(0x33);
+    });
+
+    it('cpu.sp is 0x1005', () => {
+      expect(cpu.sp).to.eql(0x1005);
+    });
   });
 });
