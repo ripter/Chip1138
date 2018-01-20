933dfd16e873128b8989b464f3c43f71eec614e6
Chris Richards <ripter001@gmail.com>
2018-01-19 17:19:27 -0800

    added sp default test

--- test/cpu.stack.test.js
+++ test/cpu.stack.test.js
@@ -5,7 +5,7 @@ import Memory from '../src/memory.js';
 // import { random8bit } from './utils.js';
 import rom from '../roms/flappyboy.json';
 
-describe('CPU Stack', () => {
+describe.only('CPU Stack', () => {
   let cpu, memory;
 
   beforeEach(() => {
@@ -15,19 +15,23 @@ describe('CPU Stack', () => {
     });
   });
 
+  it('sp defaults to 0xFFFE', () => {
+    expect(cpu.sp).to.eql(0xfffe); // largest 16-bit number
+  });
+
   /*
-    If the AF Register pair contains 2233h and the Stack Pointer contains 1007h,
+    If the AF Register pair contains 0x2233 and the Stack Pointer contains 0x1007,
     then upon the execution of a PUSH AF instruction,
-    memory address 1006h contains 22h,
-    memory address 1005h contains 33h,
-    and the Stack Pointer contains 1005h.
+    memory address 0x1006 contains 0x22,
+    memory address 0x1005 contains 0x33,
+    and the Stack Pointer contains 0x1005.
    */
 
   describe('spec example', () => {
     beforeEach(() => {
       cpu.af = 0x2233;
       cpu.sp = 0x1007;
-      cpu.processOpcode(0xf5);
+      cpu.processOpcode(0xf5); // PUSH AF;
     });
 
     it('value at address 0x1006 is 0x22', () => {
