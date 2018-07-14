0bb0fd5bb1db6143d4707e83c701ca9468013ec6
Chris Richards <ripter001@gmail.com>
2018-07-13 17:19:20 -0700

    adding some console logs to help me remember what is happening

--- test/timer.test.js
+++ test/timer.test.js
@@ -15,14 +15,20 @@ describe('timer', () => {
 
   describe.only('tick()', () => {
     it('incriments cpu.pc', () => {
+      console.log('\none tick() before, cpu.pc', `0x${cpu.pc.toString(16)}`, 'data at pc', `0x${memory.readROM(cpu.pc).toString(16)}`);
       cpu.tick();
+      console.log('\none tick() after, cpu.pc', `0x${cpu.pc.toString(16)}`, 'data at pc', `0x${memory.readROM(cpu.pc).toString(16)}`);
       expect(cpu.pc).to.eql(0x0101);
     });
 
     it('JUMP with tick()', () => {
-      // cpu.pc = 0x0101; // opcode at this address is 0xC3 JUMP
-      cpu.tick(); // Run one tick, which will run JUMP
-      cpu.tick(); // Run one tick, which will run JUMP
+      console.log('\ntwo tick() before, cpu.pc', `0x${cpu.pc.toString(16)}`, 'data at pc', `0x${memory.readROM(cpu.pc).toString(16)}`);
+      // cpu.pc = 0x0100
+      cpu.tick(); // Run one tick, which will run JUMP [0xC3]
+      console.log('\ntwo tick() after one, cpu.pc', `0x${cpu.pc.toString(16)}`, 'data at pc', `0x${memory.readROM(cpu.pc).toString(16)}`);
+      // cpu.pc = 0x0101; // opcode at this address is JUMP [0xC3]
+      cpu.tick(); // Run one tick, which will run JUMP [0xC3]
+      console.log('\ntwo tick() after both, cpu.pc', `0x${cpu.pc.toString(16)}`, 'data at pc', `0x${memory.readROM(cpu.pc).toString(16)}`);
       expect(cpu.pc).to.eql(0x0150); // The new pc after the jump.
     });
   });
