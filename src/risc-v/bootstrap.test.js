654c819b886d313f263855b41402e07c1313a5d9
Chris Richards <ripter001@gmail.com>
2019-03-21 14:12:16 -0700

    Adding a very minimual Linux compiled for RISC-V

--- src/risc-v/bootstrap.test.js
+++ src/risc-v/bootstrap.test.js
@@ -4,4 +4,15 @@ describe.only('RISC-V Bootstrap tests', () => {
   it('needs the frist test', () => {
     expect().to.fail();
   });
+
+  describe('2.2 Base Instruction Formats', () => {
+
+    it('instruction is 32 bits in length', () => {
+
+    });
+    
+    describe('R-type', () => {
+    });
+
+  });
 });
